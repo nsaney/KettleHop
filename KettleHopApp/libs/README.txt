68c7ae78dabe363f9f1568a69cb46b43fde583f5
nsaney <nsaney@gmail.com>
2015-08-30 10:59:24 -0400

    Added KettleHop for Android
    Created Android app wrapper code.

---
+++ KettleHopApp/libs/README.txt
@@ -0,0 +1,5 @@
+Step 1 of 2:
+Build the QuadradoAndroid project and copy the contents of the Deploy_QuadradoAndroid directory next to this file (into this project's /libs folder).
+
+Step 2 of 2:
+Build the KettleHop project and copy the Run_KettleHop.jar file next to this file.
---
+++ KettleHopApp/src/chairosoft/kettle_hop/KettleHopLauncherActivity.java
@@ -0,0 +1,14 @@
+package chairosoft.kettle_hop;
+
+import chairosoft.android.QuadradoLauncherActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KettleHopLauncherActivity extends QuadradoLauncherActivity
+{
+    public KettleHopLauncherActivity()
+    {
+        super(chairosoft.kettle_hop.KettleHop.class);
+    }
+}
